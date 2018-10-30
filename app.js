821fe275bf20c155a1cd03847e746e0d564105b3
jason few <omence11@gmail.com>
2018-10-30 07:27:26 -0700

    salmon1 com3

--- app.js
+++ app.js
@@ -223,3 +223,61 @@ var capHill = {
 };
 capHill.render();
 
+var alki = {
+  nameofStore: 'Alki',
+  minCusPerhr: 2,
+  maxCusPerhr: 16,
+  aveNumCookSale: 4.6,
+  openHours: [
+    '6am',
+    '7am',
+    '8am',
+    '9am',
+    '10am',
+    '11am',
+    '12pm',
+    '1pm',
+    '2pm',
+    '3pm',
+    '4pm',
+    '5pm',
+    '6pm',
+    '7pm',
+    '8pm'
+  ],
+  randomN: function() {
+    var ranNum = Math.floor(
+      Math.random() * (this.maxCusPerhr - this.minCusPerhr) + this.minCusPerhr
+    );
+    return Math.floor(ranNum * this.aveNumCookSale);
+  },
+
+  render() {
+    var container = document.createElement('section');
+    var NameStore = document.createElement('h3');
+    var salesList = document.createElement('ul');
+
+    NameStore.textContent = this.nameofStore;
+    var totalCookies = 0;
+    for (var i = 0; i < this.openHours.length; i++) {
+      var ret = this.randomN();
+      totalCookies += ret;
+      var txtOut = document.createElement('li');
+      txtOut.textContent = `${this.openHours[i]}: ${ret} cookies`;
+      salesList.appendChild(txtOut);
+    }
+    var totaltxt = document.createElement('li');
+    totaltxt.textContent = `Total: ${totalCookies} cookies`;
+    salesList.appendChild(totaltxt);
+
+    container.appendChild(NameStore);
+    container.appendChild(salesList);
+
+    var main = document.getElementById('main-content');
+    main.appendChild(container);
+  }
+};
+alki.render();
+
+
+
